b2c376ee9f6d171dc0c6fc9c5018977bd094c866
Xavier Lau <c.estlavie@icloud.com>
2021-01-06 02:09:48 +0800

    refactor: rpc & structs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
+mod primitives;
+
 // --- std ---
 use std::{
 	collections::HashMap,
-	fmt::{Debug, Display, Formatter, Result as FmtResult},
-	hash::Hasher,
+	io::{Read, Write},
 };
 // --- crates.io ---
-use base58::ToBase58;
-use blake2_rfc::blake2b::Blake2b;
-use byteorder::{ByteOrder, LittleEndian};
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 use colored::Colorize;
 use parity_scale_codec::Decode;
-use serde::{de::DeserializeOwned, Deserialize};
+use subrpcer::{grandpa, state};
+use tungstenite::{connect, Message, WebSocket};
 use serde_json::Value;
-use tungstenite::{connect, Message};
-use twox_hash::XxHash;
-
-type BlockNumber = u32;
-type QueuedKeys = Vec<(AccountId, SessionKeys)>;
+// --- grandma ---
+use primitives::*;
 
 fn main() {
 	let app = App::new(crate_name!())
@@ -29,7 +25,7 @@ fn main() {
 				.long("ws")
 				.required(true)
 				.takes_value(true)
-				.value_name("URI:PORT")
+				.value_name("URI")
 				.about("The nodes WS(S) address"),
 		)
 		.arg(
@@ -42,7 +38,7 @@ fn main() {
 				.about("The log level"),
 		);
 	let app_args = app.get_matches();
-	let ws = app_args.value_of("ws").unwrap();
+	let uri = app_args.value_of("ws").unwrap();
 	let log = match app_args.value_of("log").unwrap() {
 		"all" => 0,
 		"voted" => 1,
@@ -50,53 +46,37 @@ fn main() {
 		_ => unreachable!(),
 	};
 
-	run(ws, log);
+	run(uri, log);
 }
 
-fn run(ws: &str, log: u8) {
-	let ws = if ws.starts_with("ws://") {
-		ws.to_owned()
-	} else if ws.starts_with("wss://") {
-		ws.to_owned()
+fn run(uri: &str, log: u8) {
+	let uri = if uri.starts_with("ws://") {
+		uri.to_owned()
+	} else if uri.starts_with("wss://") {
+		uri.to_owned()
 	} else {
-		format!("ws://{}", ws)
+		format!("ws://{}", uri)
 	};
-	let (mut socket, _) = connect(ws).unwrap();
+	let (mut ws, _) = connect(uri).unwrap();
+	let spec_name = get_spec_name(&mut ws);
 
-	println!("{}", "Connected to Darwinia".green());
+	subscribe(&mut ws);
 
-	socket
-		.write_message(Message::Text(
-			r#"{"id":2,"jsonrpc":"2.0","method":"grandpa_subscribeJustifications","params":[]}"#
-				.into(),
-		))
-		.unwrap();
-	socket
-		.write_message(Message::Text(
-			format!(
-				r#"{{"id":2,"jsonrpc":"2.0","method":"state_subscribeStorage","params":[["{}"]]}}"#,
-				format!(
-					"0x{}{}",
-					hex(&twox_128(b"Session")),
-					hex(&twox_128(b"QueuedKeys"))
-				)
-			)
-			.into(),
-		))
-		.unwrap();
+	println!("{} {}", "Connected to".green(), spec_name.green());
 
 	let mut votes_map = HashMap::new();
 
 	loop {
 		if let Ok(rpc_result) =
-			serde_json::from_slice::<RpcResult>(&socket.read_message().unwrap().into_data())
+			serde_json::from_slice::<RpcResult>(&ws.read_message().unwrap().into_data())
 		{
 			match rpc_result.method.as_str() {
 				"state_storage" => {
 					let state_storage_rpc = rpc_result.into_inner::<StateStoreRpc>();
 					let session_queued_keys = state_storage_rpc.item_of(0).1;
 					let queued_keys: QueuedKeys =
-						Decode::decode(&mut &*bytes(session_queued_keys)).unwrap();
+						Decode::decode(&mut &*array_bytes::bytes_unchecked(session_queued_keys))
+							.unwrap();
 
 					votes_map.clear();
 
@@ -105,8 +85,10 @@ fn run(ws: &str, log: u8) {
 					}
 				}
 				"grandpa_justifications" => {
-					let GrandpaJustification { round, commit } =
-						Decode::decode(&mut &*bytes(&rpc_result.into_inner::<String>())).unwrap();
+					let GrandpaJustification { round, commit } = Decode::decode(
+						&mut &*array_bytes::bytes_unchecked(&rpc_result.into_inner::<String>()),
+					)
+					.unwrap();
 
 					for SignedPrecommit { id, .. } in commit.precommits {
 						votes_map.entry(id).and_modify(|(_, votes)| *votes += 1);
@@ -159,159 +141,38 @@ fn run(ws: &str, log: u8) {
 	}
 }
 
-#[derive(Debug, Deserialize)]
-struct RpcResult {
-	method: String,
-	params: Value,
-}
-impl RpcResult {
-	const RESULT: &'static str = "result";
+fn get_spec_name<Stream>(ws: &mut WebSocket<Stream>) -> String
+where
+	Stream: Read + Write,
+{
+	ws.write_message(Message::from(
+		serde_json::to_vec(&state::get_runtime_version()).unwrap(),
+	))
+	.unwrap();
 
-	fn into_inner<T: DeserializeOwned>(self) -> T {
-		serde_json::from_value(self.params[Self::RESULT].to_owned()).unwrap()
+	if let Ok(rpc_result) =
+		serde_json::from_slice::<Value>(&ws.read_message().unwrap().into_data())
+	{
+		rpc_result["result"]["specName"].as_str().unwrap().into()
+	} else {
+		"Null".into()
 	}
 }
 
-#[derive(Debug, Deserialize)]
-struct StateStoreRpc {
-	block: String,
-	changes: Vec<Vec<String>>,
-}
-impl StateStoreRpc {
-	fn key_of(&self, i: usize) -> &str {
-		&self.changes[i][0]
-	}
-
-	fn value_of(&self, i: usize) -> &str {
-		&self.changes[i][1]
-	}
-
-	fn item_of(&self, i: usize) -> (&str, &str) {
-		(self.key_of(i), self.value_of(i))
-	}
-}
-
-#[derive(Debug, Decode)]
-struct SessionKeys {
-	babe: AccountId,
-	grandpa: AccountId,
-	im_online: AccountId,
-	authority_discovery: AccountId,
-}
-
-#[derive(Debug, Decode)]
-struct GrandpaJustification {
-	round: u64,
-	commit: Commit,
-}
-
-#[derive(Debug, Decode)]
-struct Commit {
-	target_hash: Hash,
-	target_number: BlockNumber,
-	precommits: Vec<SignedPrecommit>,
-}
-
-#[derive(Debug, Decode)]
-struct SignedPrecommit {
-	precommit: Precommit,
-	signature: Signature,
-	id: AccountId,
-}
-
-#[derive(Debug, Decode)]
-struct Precommit {
-	target_hash: Hash,
-	target_number: BlockNumber,
-}
-
-#[derive(Decode)]
-struct Hash([u8; 32]);
-impl Debug for Hash {
-	fn fmt(&self, f: &mut Formatter) -> FmtResult {
-		write!(f, "Hash({})", hex(&self.0))
-	}
-}
-impl Display for Hash {
-	fn fmt(&self, f: &mut Formatter) -> FmtResult {
-		write!(f, "{}", hex(&self.0))
-	}
-}
-
-#[derive(Decode, Eq, PartialEq, Hash)]
-struct AccountId([u8; 32]);
-impl Debug for AccountId {
-	fn fmt(&self, f: &mut Formatter) -> FmtResult {
-		write!(f, "AccountId({})", into_ss58(&self.0, 18))
-	}
-}
-impl Display for AccountId {
-	fn fmt(&self, f: &mut Formatter) -> FmtResult {
-		write!(f, "{}", into_ss58(&self.0, 18))
-	}
-}
-
-#[derive(Decode)]
-struct Signature([u8; 64]);
-impl Debug for Signature {
-	fn fmt(&self, f: &mut Formatter) -> FmtResult {
-		write!(f, "Signature(omitted)")
-	}
-}
-
-fn hex(s: &[u8]) -> String {
-	s.iter()
-		.map(|c| format!("{:02x}", c))
-		.collect::<Vec<_>>()
-		.join("")
-}
-
-fn bytes(s: &str) -> Vec<u8> {
-	let s = s.trim_start_matches("0x");
-
-	(0..s.len())
-		.step_by(2)
-		.map(|i| u8::from_str_radix(&s[i..i + 2], 16).unwrap())
-		.collect()
-}
-
-fn twox_128(data: &[u8]) -> [u8; 16] {
-	let mut dest: [u8; 16] = [0; 16];
-	let r0 = {
-		let mut h0 = XxHash::with_seed(0);
-		h0.write(data);
-
-		h0.finish()
-	};
-	let r1 = {
-		let mut h1 = XxHash::with_seed(1);
-		h1.write(data);
-
-		h1.finish()
-	};
-
-	LittleEndian::write_u64(&mut dest[0..8], r0);
-	LittleEndian::write_u64(&mut dest[8..16], r1);
-
-	dest
-}
-
-fn into_ss58(bytes: &[u8], network: u8) -> String {
-	let mut account = {
-		let mut data = vec![network];
-		data.extend(bytes);
-
-		data
-	};
-
-	let blake2b = {
-		let mut context = Blake2b::new(64);
-		context.update(b"SS58PRE");
-		context.update(&account);
-
-		context.finalize()
-	};
-	account.extend(&blake2b.as_bytes()[0..2]);
-
-	account.to_base58()
+fn subscribe<Stream>(ws: &mut WebSocket<Stream>)
+where
+	Stream: Read + Write,
+{
+	ws.write_message(Message::from(
+		serde_json::to_vec(&grandpa::subscribe_justifications()).unwrap(),
+	))
+	.unwrap();
+	ws.write_message(Message::from(
+		serde_json::to_vec(&state::subscribe_storage(vec![array_bytes::hex_str(
+			"0x",
+			substorager::storage_key(b"Session", b"QueuedKeys"),
+		)]))
+		.unwrap(),
+	))
+	.unwrap();
 }
---
+++ src/primitives.rs
@@ -0,0 +1,90 @@
+// --- std ---
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+// --- crates.io ---
+use parity_scale_codec::Decode;
+use serde::{de::DeserializeOwned, Deserialize};
+use serde_json::Value;
+
+pub type BlockNumber = u32;
+pub type QueuedKeys = Vec<(AccountId, SessionKeys)>;
+pub type GrandpaJustification =
+	subgrandpa::GrandpaJustification<Hash, BlockNumber, Signature, AccountId>;
+pub type SignedPrecommit = subgrandpa::SignedPrecommit<Hash, BlockNumber, Signature, AccountId>;
+
+#[derive(Debug, Deserialize)]
+pub struct RpcResult {
+	pub method: String,
+	pub params: Value,
+}
+impl RpcResult {
+	const RESULT: &'static str = "result";
+
+	pub fn into_inner<T: DeserializeOwned>(self) -> T {
+		serde_json::from_value(self.params[Self::RESULT].to_owned()).unwrap()
+	}
+}
+
+#[derive(Debug, Deserialize)]
+pub struct StateStoreRpc {
+	pub block: String,
+	pub changes: Vec<Vec<String>>,
+}
+impl StateStoreRpc {
+	pub fn key_of(&self, i: usize) -> &str {
+		&self.changes[i][0]
+	}
+
+	pub fn value_of(&self, i: usize) -> &str {
+		&self.changes[i][1]
+	}
+
+	pub fn item_of(&self, i: usize) -> (&str, &str) {
+		(self.key_of(i), self.value_of(i))
+	}
+}
+
+#[derive(Debug, Decode)]
+pub struct SessionKeys {
+	pub babe: AccountId,
+	pub grandpa: AccountId,
+	pub im_online: AccountId,
+	pub authority_discovery: AccountId,
+}
+
+#[derive(Decode)]
+pub struct Hash(pub [u8; 32]);
+impl Debug for Hash {
+	fn fmt(&self, f: &mut Formatter) -> FmtResult {
+		write!(f, "Hash({})", array_bytes::hex_str("0x", &self.0))
+	}
+}
+impl Display for Hash {
+	fn fmt(&self, f: &mut Formatter) -> FmtResult {
+		write!(f, "{}", array_bytes::hex_str("0x", &self.0))
+	}
+}
+
+#[derive(Decode, Eq, PartialEq, Hash)]
+pub struct AccountId([u8; 32]);
+impl Debug for AccountId {
+	fn fmt(&self, f: &mut Formatter) -> FmtResult {
+		write!(
+			f,
+			"AccountId({})",
+			subcryptor::into_ss58_address(&self.0, 18)
+		)
+	}
+}
+impl Display for AccountId {
+	fn fmt(&self, f: &mut Formatter) -> FmtResult {
+		write!(f, "{}", subcryptor::into_ss58_address(&self.0, 18))
+	}
+}
+
+#[derive(Decode)]
+pub struct Signature([u8; 64]);
+impl Debug for Signature {
+	fn fmt(&self, f: &mut Formatter) -> FmtResult {
+		write!(f, "Signature(omitted)")
+	}
+}
